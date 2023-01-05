0cf1918301ffcbf7c898f99dee2d3702421338e3
Ben Demers <ben.n.demers@gmail.com>
2023-01-05 16:13:51 -0500

    add time constants

---
+++ client/src/util/constants.ts
@@ -0,0 +1,22 @@
+const weekTimes = [
+  '7:30am',
+  '8:00am',
+  '8:30am',
+  '9:00am',
+  '9:30am',
+  '10:00am',
+  '10:30am',
+  '11:00am',
+  '11:30am',
+];
+
+const weekendTimes = [
+  '9:00am',
+  '9:30am',
+  '10:00am',
+  '10:30am',
+  '11:00am',
+  '11:30am',
+];
+
+export { weekTimes, weekendTimes };
