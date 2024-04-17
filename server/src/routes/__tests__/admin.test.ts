8db86e68b981b6ca3a91d99dc848c5bde7ecb846
Ricky Raup <rickyraup@MacBook-Air-317.local>
2024-04-17 02:23:27 -0400

    try

--- server/src/routes/__tests__/admin.test.ts
+++ server/src/routes/__tests__/admin.test.ts
@@ -207,14 +207,6 @@ describe('testing admin routes', () => {
       });
     });
 
-    describe('testing GET /api/admin/adminstatus', () => {
-      it('admin calling /adminstatus is true', async () => {
-        // check admin status
-        const response = await agent.get('/api/admin/adminstatus').send();
-        expect(response.status).toBe(StatusCode.OK);
-      });
-    });
-
     describe('testing PUT /api/admin/promote', () => {
       it('admin can promote user', async () => {
         // promote user
